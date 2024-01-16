4c47af53355785488acd46db81ad8c553ea9992c
thejayadad <74875595+thejayadad@users.noreply.github.com>
2024-01-16 17:37:44 -0500

    update

--- myapp/app/dashboard/page.js
+++ myapp/app/dashboard/page.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import getServerUser from '@/lib/getServerUser'
+import React, { Suspense } from 'react'
 
-const DashboardPage = () => {
-  return (
-    <div>DashboardPage</div>
-  )
+const DashboardPage = async () => {
+    return <>
+    <Suspense fallback={<WelcomeMsgFallback />}>
+    <WelcomeMsg />
+    </Suspense>
+    </>
+  }
+  
+  export default DashboardPage
+  
+  async function WelcomeMsg(){
+      const user = await getServerUser()
+    return (
+      <h1 className='text-4xl'>
+        Welcome, <br />
+        {user.email}
+      </h1>
+    )
+  }
+  
+  function WelcomeMsgFallback(){
+    return<>
+    <h1 className='text-4xl'>
+    <div className="skeleton w-64 h-32 dark:bg-gray-800 bg-gray-200"></div>
+    </h1>
+    </>
 }
 
-export default DashboardPage
--- myapp/app/page.js
+++ myapp/app/page.js
@@ -1,9 +1,32 @@
-import Image from 'next/image'
+import getServerUser from '@/lib/getServerUser'
+import { wait } from '@/lib/wait'
+import React, { Suspense } from 'react'
 
-export default function Home() {
+const HomePage = async () => {
+  return <>
+  <Suspense fallback={<WelcomeMsgFallback />}>
+  <WelcomeMsg />
+  </Suspense>
+  </>
+}
+
+export default HomePage
+
+async function WelcomeMsg(){
+    const user = await getServerUser()
+  await wait(3000)
   return (
-    <main className='dark:bg-black dark:text-white'>
-      homePAge
-    </main>
+    <h1 className='text-4xl'>
+      Welcome, <br />
+      {user.email}
+    </h1>
   )
+}
+
+function WelcomeMsgFallback(){
+  return<>
+  <h1 className='text-4xl'>
+  <div className="skeleton w-64 h-32 dark:bg-gray-800 bg-gray-200"></div>
+  </h1>
+  </>
 }
